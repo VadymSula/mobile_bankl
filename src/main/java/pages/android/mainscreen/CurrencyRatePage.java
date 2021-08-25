e99599769aa78fb3f2e91af88be53c45f5c71740
Сула Вадим Витальевич <ext_sulavv@ubrr.ru>
2021-08-25 18:55:51 +0500

    added: locators for currency rate page

---
+++ src/main/java/pages/android/mainscreen/CurrencyRatePage.java
@@ -0,0 +1,19 @@
+package pages.android.mainscreen;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import pages.android.AndroidBasePage;
+
+public class CurrencyRatePage extends AndroidBasePage {
+    @AndroidFindBy(id = "cb.ibank:id/view_controller_currency_rates_recycler")
+    protected MobileElement CURRENCY_RATES_RECYCLER;
+
+    public CurrencyRatePage(AppiumDriver<MobileElement> appiumDriver) {
+        super(appiumDriver);
+    }
+
+    public boolean isExistCurrencyRatesRecyclerOnPage() {
+        return elements.isElementExist(CURRENCY_RATES_RECYCLER);
+    }
+}
